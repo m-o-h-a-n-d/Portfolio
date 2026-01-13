6a3187b66743c244a6e9857fdf3b0ffa72467c4c
m-o-h-a-n-d <138885574+m-o-h-a-n-d@users.noreply.github.com>
2026-01-13 09:52:11 -0500

    Fix: Rebuild loading screen with SVG motion paths for precise electron animation

--- src/components/portfolio/LoadingScreen.jsx
+++ src/components/portfolio/LoadingScreen.jsx
@@ -11,86 +11,146 @@ const LoadingScreen = () => {
       <div className="loading-content">
         {/* Animated Atom/Orbital Structure */}
         <div className="atom-container">
-          {/* Center Laravel Logo */}
-          <div className="center-logo">
-            <svg
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-              className="laravel-logo"
-            >
-              {/* Laravel 3D Logo - Golden Color */}
-              <g fill="none" stroke="currentColor" strokeWidth="0.8">
-                {/* Outer cube lines */}
-                <path d="M 6 8 L 10 5 L 14 8 L 10 11 Z" />
-                <path d="M 10 5 L 14 8 L 14 14 L 10 11 Z" />
-                <path d="M 6 8 L 6 14 L 10 11 Z" />
-                <path d="M 6 14 L 10 17 L 14 14 Z" />
-                <path d="M 14 8 L 18 11 L 18 17 L 14 14 Z" />
-                <path d="M 10 11 L 14 14 L 10 17 Z" />
+          {/* SVG with Motion Paths for Electrons */}
+          <svg
+            className="orbital-svg"
+            viewBox="0 0 300 300"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {/* Define Motion Paths for electrons */}
+            <defs>
+              {/* Orbit 1 - Horizontal ellipse */}
+              <path
+                id="orbit1"
+                d="M 150 80 Q 210 150 150 220 Q 90 150 150 80"
+                fill="none"
+              />
+              {/* Orbit 2 - Rotated 60 degrees */}
+              <path
+                id="orbit2"
+                d="M 150 80 Q 210 150 150 220 Q 90 150 150 80"
+                fill="none"
+                transform="rotate(60 150 150)"
+              />
+              {/* Orbit 3 - Rotated 120 degrees */}
+              <path
+                id="orbit3"
+                d="M 150 80 Q 210 150 150 220 Q 90 150 150 80"
+                fill="none"
+                transform="rotate(120 150 150)"
+              />
+            </defs>
+
+            {/* Orbital Paths (Visual) */}
+            <ellipse
+              cx="150"
+              cy="150"
+              rx="70"
+              ry="35"
+              fill="none"
+              stroke="url(#goldGradient)"
+              strokeWidth="2.5"
+              className="orbit-path orbit-path-1"
+            />
+            <ellipse
+              cx="150"
+              cy="150"
+              rx="70"
+              ry="35"
+              fill="none"
+              stroke="url(#goldGradient)"
+              strokeWidth="2.5"
+              className="orbit-path orbit-path-2"
+              transform="rotate(60 150 150)"
+            />
+            <ellipse
+              cx="150"
+              cy="150"
+              rx="70"
+              ry="35"
+              fill="none"
+              stroke="url(#goldGradient)"
+              strokeWidth="2.5"
+              className="orbit-path orbit-path-3"
+              transform="rotate(120 150 150)"
+            />
+
+            {/* Gradient Definition */}
+            <defs>
+              <radialGradient id="goldGradient">
+                <stop offset="0%" stopColor="#d4af37" stopOpacity="0.8" />
+                <stop offset="100%" stopColor="#d4af37" stopOpacity="0.3" />
+              </radialGradient>
+            </defs>
+
+            {/* Animated Electrons on Motion Paths */}
+            {/* Electron 1 */}
+            <g className="electron-group electron-1">
+              <animateMotion dur="6s" repeatCount="indefinite">
+                <mpath href="#orbit1" />
+              </animateMotion>
+              <circle
+                cx="0"
+                cy="0"
+                r="8"
+                fill="#d4af37"
+                className="electron-ball"
+              />
+            </g>
+
+            {/* Electron 2 */}
+            <g className="electron-group electron-2">
+              <animateMotion dur="6s" repeatCount="indefinite" keyPoints="0;1" keyTimes="0;1">
+                <mpath href="#orbit2" />
+              </animateMotion>
+              <circle
+                cx="0"
+                cy="0"
+                r="8"
+                fill="#d4af37"
+                className="electron-ball"
+              />
+            </g>
+
+            {/* Electron 3 */}
+            <g className="electron-group electron-3">
+              <animateMotion dur="6s" repeatCount="indefinite" keyPoints="0;1" keyTimes="0;1">
+                <mpath href="#orbit3" />
+              </animateMotion>
+              <circle
+                cx="0"
+                cy="0"
+                r="8"
+                fill="#d4af37"
+                className="electron-ball"
+              />
+            </g>
+
+            {/* Center Logo - Laravel 3D Cube */}
+            <g className="center-logo">
+              {/* Outer cube frame */}
+              <g fill="none" stroke="#d4af37" strokeWidth="1.2" className="logo-stroke">
+                {/* Front face */}
+                <path d="M 130 120 L 150 110 L 170 120 L 150 130 Z" />
+                {/* Top face */}
+                <path d="M 150 110 L 170 100 L 170 120 L 150 130 Z" />
+                {/* Right face */}
+                <path d="M 170 120 L 170 100 L 190 110 L 190 130 Z" />
+                {/* Left face */}
+                <path d="M 130 120 L 110 110 L 110 130 L 130 140 Z" />
+                {/* Bottom connections */}
+                <path d="M 130 140 L 150 150 L 170 140" />
+                <path d="M 150 130 L 150 150" />
               </g>
-            </svg>
-          </div>
-
-          {/* Orbital Paths (3 ellipses at different angles) */}
-          <svg className="orbits" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-            {/* Orbit 1 - Horizontal */}
-            <ellipse
-              cx="100"
-              cy="100"
-              rx="70"
-              ry="35"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              className="orbit orbit-1"
-            />
-
-            {/* Orbit 2 - Rotated 60 degrees */}
-            <ellipse
-              cx="100"
-              cy="100"
-              rx="70"
-              ry="35"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              className="orbit orbit-2"
-              transform="rotate(60 100 100)"
-            />
-
-            {/* Orbit 3 - Rotated 120 degrees */}
-            <ellipse
-              cx="100"
-              cy="100"
-              rx="70"
-              ry="35"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              className="orbit orbit-3"
-              transform="rotate(120 100 100)"
-            />
+              {/* Inner details */}
+              <g fill="none" stroke="#d4af37" strokeWidth="0.8" opacity="0.7">
+                <path d="M 140 125 L 160 125" />
+                <path d="M 150 120 L 150 135" />
+              </g>
+            </g>
           </svg>
 
-          {/* Animated Electrons (Glowing Balls) */}
-          <div className="electrons">
-            {/* Electron 1 - Orbit 1 */}
-            <div className="electron electron-1">
-              <div className="electron-ball"></div>
-            </div>
-
-            {/* Electron 2 - Orbit 2 */}
-            <div className="electron electron-2">
-              <div className="electron-ball"></div>
-            </div>
-
-            {/* Electron 3 - Orbit 3 */}
-            <div className="electron electron-3">
-              <div className="electron-ball"></div>
-            </div>
-          </div>
-
-          {/* Decorative Elements (Code-like marks) */}
+          {/* Decorative Code-like Marks */}
           <div className="decorative-marks">
             <div className="mark mark-1"></div>
             <div className="mark mark-2"></div>
