fd11f625fd9ad5fe12632cb7c5cb9101f67ce39c
Mohanad <mohandahmed015529.222.00@gmail.com>
2026-01-30 11:55:49 +0200

    First Commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -17,6 +17,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+  
 
   useEffect(() => {
     // Check if user is authenticated on mount
